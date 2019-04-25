5b2c0ef375258a4a9746c7e6d3ee92689a226f5d
Gauresh <gaureshp2@gmail.com>
2019-04-25 16:17:32 +0530

    dealing with comments with some basic info regarding variables

--- variables.php
+++ variables.php
@@ -1,4 +1,19 @@
 <?php
+    // single line comment
+    #  SINGLE LINE COMMENT
+    /*
+        Multiline
+        comment
+    */
+
+    #VARIABLES
+    /*
+        - prefix $
+        - Start with a letter and underscore
+        - only letters, numbers and underscores
+        - Case Sensitive
+    */
+
     $output = "Hello World!";
     echo $output;
 ?>
