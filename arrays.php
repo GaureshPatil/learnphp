8eb13a6feea6cf82748fb56a941e37a00e8ff0bb
Gauresh <gaureshp2@gmail.com>
2019-04-25 17:15:16 +0530

    working with Associative arrays

--- arrays.php
+++ arrays.php
@@ -10,12 +10,19 @@
     // Indexed
 
     $people = array('gauresh', 'pritesh', 'amar');
-    echo $people[1];
+    // echo $people[1];
     $ids = array(12, 15, 18);
-    echo "<br>" . $ids[2];
-    echo "<br>" . count($ids);
-    echo "<br>";
-    print_r($ids);
-    var_dump($people);
+    // echo "<br>" . $ids[2];
+    // echo "<br>" . count($ids);
+    // echo "<br>";
+    // print_r($ids);
+    // var_dump($people);
 
+    // Associative array
+    $people = array(
+        'gauresh' => 21,
+        'pritesh' => 20,
+        'amar' => 45
+    );
+    echo $people['gauresh'];
 ?>
