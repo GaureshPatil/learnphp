4a113636281ae723915a9a4ff0167a27a8ea4284
Gauresh <gaureshp2@gmail.com>
2019-04-25 17:32:42 +0530

    Dealing with for loops

---
+++ loops.php
@@ -0,0 +1,17 @@
+<?php
+    #LOOPS - execute code set number of times
+
+    /*
+        For
+        While
+        Do..While
+        Foreach
+    */
+
+    # For Loop
+    # @params - init, condiiton, inc
+
+    for ($i = 0; $i < 10; $i++) {
+        echo $i . "<br>";
+    }
+?>
