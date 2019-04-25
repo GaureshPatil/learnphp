44e80aafec048d182a10ad4082cbfff19782fa98
Gauresh <gaureshp2@gmail.com>
2019-04-25 16:59:49 +0530

    Working with Arrays and their types

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<?
+    #ARRAY - variable that holds multiple value
+
+    /*
+        - Indexed
+        - Associative
+        - Multi-dimensional
+    */
+
+    // Indexed
+
+    $people = array('gauresh', 'pritesh', 'amar');
+    echo $people[9];
+    $ids = array(12, 15, 18);
+    echo $ids[2];
+    echo count($ids);
+    print_r($ids);
+
+?>
