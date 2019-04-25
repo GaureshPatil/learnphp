bcd636e9ceea77443e61b630ebfe9bbe031f2c85
Gauresh <gaureshp2@gmail.com>
2019-04-25 17:52:57 +0530

    Creating user defined functions in PHP and calling the functions as well to see the outputs and studied about cases and mostly gonna use camelCase

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+    # FUNCTION - block of code that can be repeatedly called
+
+    /*
+        Camel Case - myFunction()
+        Lower Case - my_function()
+        Pascal Case - MyFunction() - Usually used for classes
+    */
+
+    // create simple function
+    function simpleFunction() {
+        echo "hello World!";
+    }
+    // run simple function / call simple function wherever required
+    simpleFunction();
+?>
