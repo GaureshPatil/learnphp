c6d9ba68b5d7b8999f4e05c165e4840850bcf357
Gauresh <gaureshp2@gmail.com>
2019-04-26 11:07:30 +0530

    Worked with the SUPERGLOBAL's Provided By PHP currently working with only  superglobal there are more supreglobals

---
+++ web2/server-info.php
@@ -0,0 +1,19 @@
+<?php
+    # $_SERVER SUPERGLOBAL
+
+    // Create server array
+    $server = [
+        'Host Server Name' => $_SERVER['SERVER_NAME'],
+        'Host Header' => $_SERVER['HTTP_HOST'],
+        'Server Software' => $_SERVER['SERVER_SOFTWARE'],
+        'Document Root' => $_SERVER['DOCUMENT_ROOT'],
+        'Current Page' => $_SERVER['PHP_SELF'],
+        'Script Name' => $_SERVER['SCRIPT_NAME']
+    ];
+
+    // echo $server['Host Server Name'] . "<br>";
+    // echo $server['Host Header'] . "<br>";
+    // echo $server['Server Software'];
+    print_r($server);
+    // Create a client array
+?>
