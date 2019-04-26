8e80601a10be4b8fe3d466003fe174401113c1df
Gauresh <gaureshp2@gmail.com>
2019-04-26 11:53:35 +0530

    Working with basic form using Superglobals GET AND POST with redirection to the samepage and also securing stuff from attack XSS

---
+++ get_post.php
@@ -0,0 +1,29 @@
+<?php 
+    if(isset($_GET['name'])) {
+        $name = htmlentities($_GET['name']);
+        echo $name;
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+</head>
+<body>
+    <form action="get_post.php" method="get">
+        <div>
+            <label>Name</label><br>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label>Email</label><br>
+            <input type="text" name="email">
+        </div>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
