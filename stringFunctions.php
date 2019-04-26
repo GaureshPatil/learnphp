f36138a2b96001922bec6bdbbd23b155da624c6a
Gauresh <gaureshp2@gmail.com>
2019-04-26 12:23:31 +0530

    Implemented string functions well as you can see there is only one but i knw all of the other functions as well if you want you can check the tutorials

---
+++ stringFunctions.php
@@ -0,0 +1,12 @@
+<?php 
+    # substr()
+    # Returns Portion of String
+
+    $output = substr("Hello", 1);
+    echo $output;
+
+    // strtoUpper() - tolower() - ucwords() - Upper case and Lowercase for strings
+    // strlen() - to find lenght of string
+    // gzcompress gzuncompress -  compressing the strings
+    // and many more there for manipulating
+?>
