14d8652f5876482c80d5eb37ee2b3e117243ccdc
Gauresh <gaureshp2@gmail.com>
2019-04-29 15:21:14 +0530

    learned inherintence concept in OOP

--- oop.php
+++ oop.php
@@ -26,11 +26,34 @@
 
     }
 
-    $person1 = new person('gauresh patil', 'gauresh@gmail.com');
+    // $person1 = new person('gauresh patil', 'gauresh@gmail.com');
     // $person1 -> name = "Gauresh Patil";
     // echo $person1 -> name;
 
     // $person1 -> setName('Gauresh Patil');
-    echo $person1->getName();
-    echo $person1->getEmail();
+    // echo $person1->getName();
+    // echo $person1->getEmail();
+
+    class Customer extends person {
+        private $balance;
+        
+        public function __construct($name, $email, $balance) {
+            parent::__construct($name, $email, $balance);
+            $this->balance = $balance;
+            echo 'A new '. __CLASS__ . 'has been created <br>';
+        }
+
+        public function setBalance($balance) {
+            $this->balance = $balance;
+        }
+
+        public function getBalance() {
+            return $this->balance;
+        }
+    }
+
+    $customer1 = new Customer('gp', 'gp@gmail.com', 300);
+
+    echo $customer1->getBalance();
+
 ?>
