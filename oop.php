bc2dba626c31af42b1186c3a31882137ca50cd34
Gauresh <gaureshp2@gmail.com>
2019-04-29 12:32:08 +0530

    learned about constructor and destructor in oop php

--- oop.php
+++ oop.php
@@ -3,20 +3,34 @@
         private $name;
         private $email;
 
+        public function __construct($name, $email) {
+            $this ->name = $name;
+            $this ->email = $email;
+            echo 'Person Created <br>';
+        }
         public function setName($name) {
             $this -> name = $name;
         }
 
         public function getName() {
-            return $this -> name;
+            return $this -> name . '<br>';
+        }
+
+        public function setEmail($email) {
+            $this -> email = $email;
+        }
+
+        public function getEmail() {
+            return $this->email . "<br>";
         }
 
     }
 
-    $person1 = new person;
+    $person1 = new person('gauresh patil', 'gauresh@gmail.com');
     // $person1 -> name = "Gauresh Patil";
     // echo $person1 -> name;
 
-    $person1 -> setName('Gauresh Patil');
+    // $person1 -> setName('Gauresh Patil');
     echo $person1->getName();
+    echo $person1->getEmail();
 ?>
