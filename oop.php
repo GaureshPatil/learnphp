2b42aaf7ead807acd8584f55655228651fd31d52
Gauresh <gaureshp2@gmail.com>
2019-04-29 12:03:07 +0530

    learning object oriented programming which will be very useful for learning MVC

---
+++ oop.php
@@ -0,0 +1,11 @@
+<?php
+    class person {
+        public $name;
+        public $email;
+
+    }
+
+    $person1 = new person;
+    $person1 -> name = "Gauresh Patil";
+    echo $person1 -> name;
+?>
