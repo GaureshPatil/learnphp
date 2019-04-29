6945af8005e7bbc6c4228ce2f898b8b8539ab1bf
Gauresh <gaureshp2@gmail.com>
2019-04-29 12:17:58 +0530

    learned about getter and setter method or functions and implemented it

--- oop.php
+++ oop.php
@@ -1,11 +1,22 @@
 <?php
     class person {
-        public $name;
-        public $email;
+        private $name;
+        private $email;
+
+        public function setName($name) {
+            $this -> name = $name;
+        }
+
+        public function getName() {
+            return $this -> name;
+        }
 
     }
 
     $person1 = new person;
-    $person1 -> name = "Gauresh Patil";
-    echo $person1 -> name;
+    // $person1 -> name = "Gauresh Patil";
+    // echo $person1 -> name;
+
+    $person1 -> setName('Gauresh Patil');
+    echo $person1->getName();
 ?>
