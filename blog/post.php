20f9bd369b8d3d30fc055b9b81cea232a1cff398
Gauresh <gaureshp2@gmail.com>
2019-04-30 15:44:06 +0530

    added two buttons for editing the post as well as deleting the post

--- blog/post.php
+++ blog/post.php
@@ -2,6 +2,21 @@
     require('config/config.php');
     require('config/db.php');
 
+     // Check for Submit
+    if (isset($_POST['delete'])) {
+        // echo "submitted";
+        // get form data
+        $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']); 
+
+        $query = "DELETE FROM posts WHERE id = {$delete_id}";
+
+        if (mysqli_query($conn, $query)) {
+            header('Location: ' . ROOT_URL . '');
+        } else {
+            echo "ERROR: " . mysqli_error($conn);
+        }
+    }
+
     // Get ID
     $id = mysqli_real_escape_string($conn, $_GET['id']);
 
@@ -30,5 +45,12 @@
         <small>Created On <?php echo $post['created_at']; ?> By 
         <?php echo $post['author']; ?></small>
         <p><?php echo $post['body']; ?></p>
+        <form class="pull-right" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="delete_id" value="<?php echo $post['id']; ?>">
+            <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+        </form>
+
+        <hr>
+        <a href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['id']; ?>" class="btn btn-warning">Edit</a>
     </div>
 <?php include("inc/footer.php"); ?>
